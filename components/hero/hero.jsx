65dfadad145dcd8ab30b9f5a577ae5b66d4654f0
Aznim Rahman <aznimr@protonmail.com>
2025-05-21 20:48:04 +0100

    convert ascii art to js

--- components/hero/hero.jsx
+++ components/hero/hero.jsx
@@ -1,10 +1,16 @@
 import BlockText from "@/components/ascii/block";
-
+import Tree from "@/components/ascii/tree";
 export const Hero = () => {
   return (
-    <section>
-      hero section here
-      <pre>{BlockText}</pre>
+    <section className="h-full">
+      <div>
+        <p>hero section here</p>
+      </div>
+
+      <div className="">
+        <pre className="w-1/2">{Tree}</pre>
+        <pre className="w-1/2">{BlockText}</pre>
+      </div>
     </section>
   );
 };
--- components/layout/footer.jsx
+++ components/layout/footer.jsx
@@ -1,6 +1,6 @@
 export const Footer = () => {
   return (
-    <footer className="mt-auto w-full bg-gray-200 flex items-center justify-center p-6">
+    <footer className="mt-auto w-full bg-gray-200 flex items-center justify-center p-4">
       <div className="flex items-center gap-4">
         <p>github</p>
         <p>linkedin</p>
