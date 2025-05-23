bcc123decee552dfa5c4502711e4711678aa767b
Aznim Rahman <aznimr@protonmail.com>
2025-05-23 12:37:57 +0100

    add partial data

---
+++ utils/data.js
@@ -0,0 +1,14 @@
+export const components = [
+  { id: 1, name: "", icon: "", featured: true },
+  { id: 2, name: "", icon: "", featured: true },
+  { id: 3, name: "", icon: "", featured: true },
+  { id: 4, name: "", icon: "", featured: false },
+  { id: 5, name: "", icon: "", featured: false },
+  { id: 6, name: "", icon: "", featured: true },
+  { id: 7, name: "", icon: "", featured: false },
+  { id: 8, name: "", icon: "", featured: false },
+  { id: 9, name: "", icon: "", featured: false },
+  { id: 10, name: "", icon: "", featured: true },
+  { id: 11, name: "", icon: "", featured: true },
+  { id: 12, name: "", icon: "", featured: false },
+];
