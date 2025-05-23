2e9f38ee77b3e8be46b32732e662b1444ad25d63
Aznim Rahman <aznimr@protonmail.com>
2025-05-23 20:38:34 +0100

    add icons

---
+++ components/svg/npm-icon.tsx
@@ -0,0 +1,14 @@
+export const NpmIcon = ({ ...props }) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="20"
+    height="20"
+    viewBox="0 0 24 24"
+    {...props}
+  >
+    <path
+      fill="currentColor"
+      d="M22 2V1H2v1H1v20h1v1h20v-1h1V2zM12 8v11H5V5h14v14h-3V8z"
+    />
+  </svg>
+);
