a926fc2ed1b9a185869abf9dafd73a7c1afd3b5a
Aznim Rahman <aznimr@protonmail.com>
2025-05-23 20:42:26 +0100

    change check and copy icons size

--- components/svg/copy-icon.tsx
+++ components/svg/copy-icon.tsx
@@ -1,8 +1,8 @@
 export const CopyIcon = ({ ...props }) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
-    width="14"
-    height="14"
+    width="18"
+    height="18"
     viewBox="0 0 24 24"
     {...props}
   >
