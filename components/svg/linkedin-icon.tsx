2da153cf10126b58ca00fcb911940467606cb24e
Aznim Rahman <aznimr@protonmail.com>
2025-05-25 11:03:58 +0100

    fix linkedin icon

--- components/svg/linkedin-icon.tsx
+++ components/svg/linkedin-icon.tsx
@@ -3,8 +3,8 @@ export const LinkedinIcon = ({ ...props }) => (
     role="presentation"
     aria-hidden="true"
     xmlns="http://www.w3.org/2000/svg"
-    width="20"
-    height="20"
+    width="21"
+    height="21"
     viewBox="0 0 24 24"
     {...props}
   >
