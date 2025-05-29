1c27106c4f1124919aff541554635dc33dc74d42
Aznim Rahman <aznimr@protonmail.com>
2025-05-29 13:00:57 +0100

    install react syntax highlighter and fix navlinks deco bullet

--- components/layout/header.jsx
+++ components/layout/header.jsx
@@ -1,33 +1,40 @@
-"use client";
+'use client'
 
-import { cn } from "@/utils";
-import { useThemeColorChange } from "@/utils/hooks";
-import Link from "next/link";
-import { MobileNav, NavLinks } from "../nav";
+import { cn } from '@/utils'
+import { useThemeColorChange } from '@/utils/hooks'
+import Link from 'next/link'
+import { MobileNav, NavLinks } from '../nav'
 
 export const Header = () => {
-  const { pageClass } = useThemeColorChange();
+    const { pageClass } = useThemeColorChange()
 
-  return (
-    <nav role="navigation" className={cn(pageClass)}>
-      <div className="flex justify-between items-center py-2 mx-6">
-        {/* Home */}
-        <div className={cn("flex items-center gap-4 nav-logo", pageClass)}>
-          <Link href="/" className="monoton text-xl">
-            Divy
-          </Link>
+    return (
+        <nav role="navigation" className={cn(pageClass)}>
+            <div className="mx-6 flex items-center justify-between py-2">
+                {/* Home */}
+                <div
+                    className={cn(
+                        'nav-logo flex items-center gap-4',
+                        pageClass
+                    )}
+                >
+                    <Link href="/" className="monoton text-xl">
+                        Divy
+                    </Link>
 
-          <span className={cn("nunito-light version-pill", pageClass)}>
-            v.3.0
-          </span>
-        </div>
+                    <span
+                        className={cn('nunito-light version-pill', pageClass)}
+                    >
+                        v.3.0
+                    </span>
+                </div>
 
-        {/* Menu */}
-        <NavLinks />
+                {/* Menu */}
+                <NavLinks />
 
-        {/* Mobile nav */}
-        <MobileNav />
-      </div>
-    </nav>
-  );
-};
+                {/* Mobile nav */}
+                <MobileNav />
+            </div>
+        </nav>
+    )
+}
--- components/nav/nav-links.js
+++ components/nav/nav-links.js
@@ -1,33 +1,34 @@
-import { cn, navMenu, useThemeColorChange } from "@/utils";
-import Link from "next/link";
+import { cn, navMenu, useThemeColorChange } from '@/utils'
+import Link from 'next/link'
 
 export const NavLinks = () => {
-  const { pathname, pageClass } = useThemeColorChange();
+    const { pathname, pageClass } = useThemeColorChange()
 
-  return (
-    <ul
-      className={cn(
-        "hidden lg:flex gap-6 nunito-regular capitalize",
-        pageClass
-      )}
-    >
-      {navMenu.map(({ id, href, name }) => {
-        const isActive =
-          pathname === href ||
-          (name === "components" && pathname.startsWith("/docs/components"));
-        return (
-          <li key={id}>
-            <Link
-              href={href}
-              className={cn("transition-link py-1", {
-                active: isActive,
-              })}
-            >
-              {name}
-            </Link>
-          </li>
-        );
-      })}
-    </ul>
-  );
-};
+    return (
+        <ul
+            className={cn(
+                'nunito-regular hidden gap-4 capitalize lg:flex',
+                pageClass
+            )}
+        >
+            {navMenu.map(({ id, href, name }) => {
+                const isActive =
+                    pathname === href ||
+                    (name === 'components' &&
+                        pathname.startsWith('/docs/components'))
+                return (
+                    <li key={id}>
+                        <Link
+                            href={href}
+                            className={cn('transition-link px-3 py-1', {
+                                active: isActive,
+                            })}
+                        >
+                            {name}
+                        </Link>
+                    </li>
+                )
+            })}
+        </ul>
+    )
+}
